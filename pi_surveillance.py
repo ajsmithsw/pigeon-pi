c2cfe74c9e4fc8a8030e057fe696d95ef1fa3c2c
Alex Smith <ajsmithsw@gmail.com>
2020-04-18 15:20:18 +0100

    Motion detection with LED indication and video feed

--- pi_surveillance.py
+++ pi_surveillance.py
@@ -1,13 +1,14 @@
-from pyimagesearch.tempimage import TempImage
+# from pyimagesearch.tempimage import TempImage
 from picamera.array import PiRGBArray
 from picamera import PiCamera
+from gpiozero import LED
 import argparse
 import warnings
 import datetime
+import cv2
 import imutils
 import json
 import time
-import cv2
 
 # Argument parser
 ap = argparse.ArgumentParser()
@@ -18,22 +19,27 @@ args = vars(ap.parse_args())
 warnings.filterwarnings("ignore")
 conf = json.load(open(args["conf"]))
 
+# Setup LED indicators (optional)
+blue = LED(17)
+white = LED(27)
+
 # Setup camera
 camera = PiCamera()
 camera.resolution = tuple(conf["resolution"])
 camera.framerate = conf["fps"]
-rawCapture = PiRGBArray(camera, size=tuple(conf["resolution"]))
+stream = PiRGBArray(camera, size=tuple(conf["resolution"]))
 
 # Allow camera to warm up, initialize average frame, last
 # uploaded timestamp, and frame motion counter
 print("[INFO] warming up...")
 time.sleep(conf["camera_warmup_time"])
+blue.on()
 avg = None
 lastUploaded = datetime.datetime.now()
 motionCounter = 0
 
 # capture the frames from the camera
-for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+for f in camera.capture_continuous(stream, format="bgr", use_video_port=True):
     frame = f.array
     timestamp = datetime.datetime.now()
     text = ""
@@ -46,7 +52,7 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     if avg is None:
         print("[INFO] starting background model...")
         avg = gray.copy().astype("float")
-        rawCapture.truncate(0)
+        stream.truncate(0)
         continue
 
     cv2.accumulateWeighted(gray, avg, 0.5)
@@ -59,14 +65,28 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
 
     for c in contours:
         if cv2.contourArea(c) < conf["min_area"]:
+            motion_detected = False
+            white.off()
             continue
 
         #compute bounding box
         (x, y, w, h) = cv2.boundingRect(c)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         motion_detected = True
+        white.on()
 
     ts = timestamp.strftime("%A %d %B %Y %I:%M:%Sp")
     cv2.putText(frame, "Motion Detected", (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    
+    # Show video feed
+    if conf["show_video"]:
+        cv2.imshow("Feed", frame)
+        
+        # Quit using the 'q' key
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+        
+    stream.truncate(0)
 
